d3b64278ca43511cf464109a120e4f08660c065b
Martyna Habior <martyna.habior@gmail.com>
2019-01-19 10:14:57 +0100

    Updates

---
+++ precache-manifest.7a142524c3cc5b21bb2737a0ba43c93e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f80ae3bf1cbcda5dad1",
+    "url": "/random/static/js/runtime~main.5f80ae3b.js"
+  },
+  {
+    "revision": "9e89b373efd37815212e",
+    "url": "/random/static/js/main.9e89b373.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/random/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "9e89b373efd37815212e",
+    "url": "/random/static/css/main.f8cc6f1b.chunk.css"
+  },
+  {
+    "revision": "21dab8a9bc01eba3a30d075ab15506c1",
+    "url": "/random/index.html"
+  }
+];
